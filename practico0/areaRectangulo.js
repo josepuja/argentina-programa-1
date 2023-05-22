eab28cfc7f20bd71586b48e2b994b39688e77393
josepuja <alfiamuebles@gmail.com>
2023-05-22 19:55:42 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,10 @@
+// parte a
+let base = 5
+let altura = 3
+let area
+console.log(base * altura);
+// parte b
+base = 12.5
+altura = 5.3
+area
+console.log(base * altura);
---
+++ practico0/conversionGrados.js
@@ -0,0 +1,10 @@
+// parte a
+let celsius  
+let fahrenheit
+fahrenheit = 50
+celsius = (fahrenheit - 32) * (5/9)
+console.log(celsius);
+// parte b
+fahrenheit = 75
+celsius = (fahrenheit - 32) * (5/9)
+console.log(celsius);

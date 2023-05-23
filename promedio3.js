eaa6f9097f6c472adf1239a25ff6b25308605c57
josepuja <134016364+josepuja@users.noreply.github.com>
2023-05-23 20:33:52 -0300

    Add files via upload

---
+++ promedio3.js
@@ -0,0 +1,24 @@
+// parte a
+
+let numero1 
+let numero2 
+let numero3 
+
+
+numero1 = 5
+numero2 = 7
+numero3 = 9
+let promedio = ((numero1+numero2+numero3)/3);
+console.log (promedio);
+// parte b
+numero1 = -2
+numero2 = -3
+numero3 = -8
+promedio = ((numero1+numero2+numero3)/3);
+console.log (promedio);
+//parte c
+numero1 = -1.2
+numero2 = 5.3
+numero3 = -3.3
+promedio = ((numero1+numero2+numero3)/3);
+console.log (promedio);

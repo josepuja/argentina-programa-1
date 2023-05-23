37611dca6237e6d599978a367f0b128ca683133d
josepuja <alfiamuebles@gmail.com>
2023-05-23 20:27:14 -0300

    subir promedio3

--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -8,15 +8,17 @@ let numero3
 numero1 = 5
 numero2 = 7
 numero3 = 9
-console.log((numero1+numero2+numero3)/3);
+let promedio = ((numero1+numero2+numero3)/3);
+console.log (promedio);
 // parte b
 numero1 = -2
 numero2 = -3
 numero3 = -8
-
-console.log((numero1+numero2+numero3)/3);
+promedio = ((numero1+numero2+numero3)/3);
+console.log (promedio);
 //parte c
 numero1 = -1.2
 numero2 = 5.3
 numero3 = -3.3
-console.log((numero1+numero2+numero3)/3);
+promedio = ((numero1+numero2+numero3)/3);
+console.log (promedio);
